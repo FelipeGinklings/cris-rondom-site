5e7d069c0437ae75d9b3036c1332dbc3db2ad8c1
Yasmin <153131852+yasminfaccin@users.noreply.github.com>
2025-11-30 12:12:03 -0300

    Fix: ajustado texto da aba promoções

--- src/components/portifolio/Others.tsx
+++ src/components/portifolio/Others.tsx
@@ -11,7 +11,9 @@ const Others = () => {
                     Promoções
                 </h3>
                 <p className="text-lg text-gray-700">
-                    Mais informações sobre serviços adicionais e diferenciais.
+                    O início de um novo ano é um convite para renovação e autodescoberta. Que tal começar 2026 se presenteando com uma experiência relaxante e inesquecível?<br>
+                    </br><br></br> Nossas sessões especiais estão aqui para ajudá-la a dar as boas-vindas a este novo ciclo, cheio de energias positivas e bem-estar. Imagine começar o ano renovado, dando ao seu corpo o cuidado que ele merece!
+                    <br></br><br></br> Estamos prontos para tornar essa experiência única e especial para você. Entre em contato e agende seu momento de autocuidado hoje mesmo. Vamos juntos iniciar o ano com vitalidade e serenidade!
                 </p>
             </div>
         </section>
